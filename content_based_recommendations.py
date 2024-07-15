f80acd01f04e64597c9c79851b13c856e6052ace
VietThanh2002 <114978891+VietThanh2002@users.noreply.github.com>
2024-07-15 21:42:00 +0700

    update models

--- content_based_recommendations.py
+++ content_based_recommendations.py
@@ -14,7 +14,14 @@ else:
     print("Không thể kết nối đến cơ sở dữ liệu")
 
 # Truy vấn để lấy dữ liệu từ bảng products
-query = "SELECT id, name, des, category_id, sub_category_id FROM products"
+query = """
+    SELECT p.id, p.name, p.des, p.category_id, p.sub_category_id, p.brand_id, 
+        b.name as brand_name, c.name as category_name, sc.name as sub_category_name
+    FROM products p
+    LEFT JOIN brands b ON p.brand_id = b.id
+    LEFT JOIN categories c ON p.category_id = c.id
+    LEFT JOIN sub_categories sc ON p.sub_category_id = sc.id
+"""
 df_products = pd.read_sql(query, db_connection.connection)
 
 # Đóng kết nối
@@ -30,16 +37,22 @@ def clean_html(raw_html):
 df_products['clean_des'] = df_products['des'].apply(clean_html)
 
 # Gợi ý dựa trên nội dung
-def content_based_recommendations(product_id, num_recommendations=5):
+def content_based_recommendations(product_id, num_recommendations=3):
     tf = TfidfVectorizer(stop_words='english', max_df=0.8, min_df=5, ngram_range=(1, 2))
-    df_products['content'] = df_products['clean_des'] + ' ' + df_products['category_id'].astype(str) + ' ' + df_products['sub_category_id'].astype(str)
+    df_products['content'] = (
+        df_products['clean_des'] + ' ' + 
+        df_products['category_name'].astype(str) + ' ' + 
+        df_products['sub_category_name'].astype(str) + ' ' +
+        df_products['brand_name'].astype(str)
+    )
     tf_matrix = tf.fit_transform(df_products['content'])
     cosine_sim = cosine_similarity(tf_matrix, tf_matrix)
+    # print(cosine_sim)
     
     idx = df_products.index[df_products['id'] == product_id][0]
     sim_scores = list(enumerate(cosine_sim[idx]))
-    print(sim_scores)
     sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
+    print(sim_scores)
     sim_scores = sim_scores[1:num_recommendations+1]
     
     product_indices = [i[0] for i in sim_scores]
--- text.py
+++ text.py
@@ -5,7 +5,14 @@ from db_connect import dbConnect
 db = dbConnect()
 
 # Thực hiện truy vấn
-query = "SELECT id, name, slug FROM products"
+query = """
+    SELECT p.id, p.name, p.des, p.category_id, p.sub_category_id, p.brand_id, 
+        b.name as brand_name, c.name as category_name, sc.name as sub_category_name
+    FROM products p
+    LEFT JOIN brands b ON p.brand_id = b.id
+    LEFT JOIN categories c ON p.category_id = c.id
+    LEFT JOIN sub_categories sc ON p.sub_category_id = sc.id
+"""
 
 db.execute_query(query)
 
