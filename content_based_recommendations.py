a46ad58de48997a0a541c8a34e48285b484ec94c
VietThanh2002 <114978891+VietThanh2002@users.noreply.github.com>
2024-07-17 22:13:03 +0700

    update models

--- content_based_recommendations.py
+++ content_based_recommendations.py
@@ -2,6 +2,7 @@ from db_connect import dbConnect
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
+from flask import Flask, request, jsonify
 from bs4 import BeautifulSoup
 
 # Kết nối đến cơ sở dữ liệu
@@ -15,12 +16,10 @@ else:
 
 # Truy vấn để lấy dữ liệu từ bảng products
 query = """
-    SELECT p.id, p.name, p.des, p.category_id, p.sub_category_id, p.brand_id, 
-        b.name as brand_name, c.name as category_name, sc.name as sub_category_name
+    SELECT p.id, p.name as product_name, p.des, b.name as brand_name, c.name as category_name
     FROM products p
     LEFT JOIN brands b ON p.brand_id = b.id
     LEFT JOIN categories c ON p.category_id = c.id
-    LEFT JOIN sub_categories sc ON p.sub_category_id = sc.id
 """
 df_products = pd.read_sql(query, db_connection.connection)
 
@@ -40,9 +39,9 @@ df_products['clean_des'] = df_products['des'].apply(clean_html)
 def content_based_recommendations(product_id, num_recommendations=3):
     tf = TfidfVectorizer(stop_words='english', max_df=0.8, min_df=5, ngram_range=(1, 2))
     df_products['content'] = (
+        df_products['product_name'] + ' ' + 
         df_products['clean_des'] + ' ' + 
         df_products['category_name'].astype(str) + ' ' + 
-        df_products['sub_category_name'].astype(str) + ' ' +
         df_products['brand_name'].astype(str)
     )
     tf_matrix = tf.fit_transform(df_products['content'])
@@ -58,5 +57,18 @@ def content_based_recommendations(product_id, num_recommendations=3):
     product_indices = [i[0] for i in sim_scores]
     return df_products.iloc[product_indices]
 
-print("Content-based Recommendations:")
-print(content_based_recommendations(30))
+# print("Content-based Recommendations:")
+# print(content_based_recommendations(30))
+# Endpoint API để lấy gợi ý sản phẩm
+app = Flask(__name__)
+
+@app.route('/recommendations', methods=['GET'])
+def recommendations():
+    product_id = int(request.args.get('product_id'))
+    recommended_products = content_based_recommendations(product_id)
+    recommended_product_ids = recommended_products['id'].tolist()
+    data = {'recommended_product_ids': recommended_product_ids}
+    return jsonify(data)
+
+if __name__ == '__main__':
+    app.run(port=9090)
